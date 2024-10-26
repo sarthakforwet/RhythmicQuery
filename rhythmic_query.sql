629f568aed4c4b7d6a251b62de62e9f73ba7ed54
Sarthak Khandelwal <khandelwal.sar@northeastern.edu>
2024-10-25 20:09:07 -0400

    customer spending by genre

--- rhythmic_query.sql
+++ rhythmic_query.sql
@@ -104,5 +104,66 @@ desc
 limit 5;
 
 
-# What can you tell me about the our customers that are spending the most on [genre], [artist], etc?
+# What can you tell me about the customers that are spending the most on [genre], [artist], etc?
 
+# Most spending on genre
+# customer, genre, track, invoiceline
+
+-- select rank() over(partition by c.customerId, g.GenreId order by il.unitPrice * il.Quantity) as cust_rank -- count(*), 
+-- from Customer c
+-- Join 
+-- Invoice i
+-- on
+-- c.CustomerId = i.CustomerId
+-- join Invoiceline il
+-- on
+-- i.InvoiceId = il.InvoiceId
+-- join track t
+-- on
+-- t.TrackId = il.TrackId
+-- join
+-- Genre g
+-- on
+-- g.GenreId = t.GenreId
+-- -- group by
+-- -- t.GenreId
+
+WITH GenreSpending AS (
+    SELECT 
+        g.GenreId,
+        g.Name AS Genre,
+        c.CustomerId,
+        concat(c.FirstName, ' ', c.LastName) AS CustomerName,
+        SUM(il.UnitPrice * il.Quantity) AS TotalSpending
+    FROM 
+        Customer c
+    JOIN 
+        Invoice i ON c.CustomerId = i.CustomerId
+    JOIN 
+        InvoiceLine il ON i.InvoiceId = il.InvoiceId
+    JOIN 
+        Track t ON t.TrackId = il.TrackId
+    JOIN 
+        Genre g ON g.GenreId = t.GenreId
+    GROUP BY 
+        g.GenreId, c.CustomerId
+),
+RankedSpending AS (
+    SELECT 
+        GenreId,
+        Genre,
+        CustomerId,
+        CustomerName,
+        TotalSpending,
+        RANK() OVER (PARTITION BY GenreId ORDER BY TotalSpending DESC) AS SpendingRank
+    FROM 
+        GenreSpending
+)
+SELECT 
+    Genre,
+    CustomerName AS TopSpender,
+    TotalSpending
+FROM 
+    RankedSpending
+WHERE 
+    SpendingRank = 1;
